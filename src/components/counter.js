8017843b48087ca61fcfd7fe31acf65bdcc6f43f
ennouri0maak <ben10ennouri@gmail.com>
2020-07-30 11:06:26 +0100

    first commit

---
+++ src/components/counter.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import {
+  incrementAction as increment,
+  decrementAction as decrement,
+  resetAction as reset
+} from "../action/indexAction";
+
+class Counter extends Component {
+  increment = () => {
+    this.props.dispatch(increment());
+  };
+
+  decrement = () => {
+    this.props.dispatch(decrement());
+  };
+
+  reset = () => {
+    this.props.dispatch(reset());
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Counter</h2>
+        <button onClick={this.decrement}>-</button>
+        <span> {this.props.count} </span>
+        <button onClick={this.increment}>+</button>
+        <br />
+        <br />
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+
+
+export default connect(mapStateToProps)(Counter);
---
+++ src/components/counterTemp.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+import {
+  incrementAction,
+  decrementAction,
+  resetAction,
+} from "../action/indexAction";
+
+class Counter extends Component {
+  render() {
+    const { count, increment, decrement, reset } = this.props;
+    return (
+      <div>
+        <h2>Counter</h2>
+        <button onClick={decrement}>-</button>
+        <span> {count} </span>
+        <button onClick={increment}>+</button>
+        <br />
+        <br />
+        <button onClick={reset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state.count,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      increment: incrementAction,
+      decrement: decrementAction,
+      reset: resetAction,
+    },
+    dispatch
+  );
+}
+
+Counter = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+export default Counter;
